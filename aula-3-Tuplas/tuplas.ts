34f8fd8f650388db7d463d90eabab423176759c3
jorgeCasalini <joshcasalini@gmail.com>
2025-05-01 17:09:57 -0300

    Adds class about tuples in Typescript

---
+++ aula-3-Tuplas/tuplas.ts
@@ -0,0 +1,11 @@
+// Muito utilizada para inserir em um valor uma determinada quantidade especifica
+// e tipo especifico, por exemplo com um ponto de coordenada no plano cartesiano ( x, y )
+
+let point:[number, number]
+
+let x:number = 2
+let y:number = 5
+
+point = [x, y]
+
+//Os pontos dados forma x = 2 e y = 5
